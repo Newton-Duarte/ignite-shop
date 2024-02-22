0579aa67d55e21f71fc53b6089d5f79c51073318
Newton Duarte <newton_@hotmail.com>
2024-02-22 14:24:24 -0300

    style: add global styles and colors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { styled } from "../styles"
 
 const Button = styled('button', {
-  backgroundColor: '$rocketseat',
+  backgroundColor: '$green300',
   borderRadius: 4,
   border: 0,
   padding: '4px 8px',
@@ -18,7 +18,7 @@ const Button = styled('button', {
 export default function Home() {
   return (
     <Button>
-      <span>Testedassd</span>
+      <span>Teste</span>
       Enviar
     </Button>
   )
