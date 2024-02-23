953d702d5b3504a2df6898bbafb991d53b9e1dad
Newton Duarte <newton_@hotmail.com>
2024-02-23 11:28:58 -0300

    feat: add ssg - static site generation to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
-import { GetServerSideProps } from "next"
+import { GetStaticProps } from "next"
 import Image from "next/image"
 import { useKeenSlider } from 'keen-slider/react'
 import { HomeContainer, Product } from "../styles/pages/home"
-
-import camiseta1 from '../assets/camisetas/1.png'
-import camiseta2 from '../assets/camisetas/2.png'
-import camiseta3 from '../assets/camisetas/3.png'
 import { stripe } from "@/lib/stripe"
 
 import 'keen-slider/keen-slider.min.css'
@@ -46,7 +42,7 @@ export default function Home({ products }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   })
@@ -65,6 +61,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       products
-    }
+    },
+    revalidate: 60 * 60 * 2 // 2 hours
   }
 }
